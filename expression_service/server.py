297ed048a667cfc6ad9e5237be4fcee92ffb5a37
Pablo Frias <pablosfrias@gmail.com>
2020-04-20 22:22:02 -0300

    2nd class. Running on Docker

--- expression_service/server.py
+++ expression_service/server.py
@@ -2,6 +2,7 @@
 from flask import Flask, jsonify
 import re
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -10,7 +11,8 @@ def getSentimentValue(exp):
     if len(exp) == 0: abort(404)
     words = re.split(" ", exp)
     sentiment = 0
-    url = 'http://127.0.0.1:5000/dictionary/api/v1.0/words/'
+    ip_address = os.environ['DICTIONARY_IP']
+    url = 'http://' + ip_address + ':5000/dictionary/api/v1.0/words/'
     
     for word in words:
         response = requests.get(url + word.strip())
@@ -27,4 +29,4 @@ def not_found(error):
     return jsonify({'error': 'Expression cannot be empty'}), 404
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run(host='0.0.0.0', debug=True)
